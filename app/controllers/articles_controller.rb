aa148822c814fb72fde5694d1ca312e12e026154
shikaifeng <kaifeng15@163.com>
2014-05-05 17:11:00 +0800

    xiu gai

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,38 @@
 class ArticlesController < ApplicationController
     def new   
     end
-    def create
-        render plain: params[:article].inspect
+
+    def index
+        @articles = Article.all
     end
+
+    def show
+        @article = Article.find(params[:id])        
+    end
+
+        def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(article_params)
+
+        @article.save
+        redirect_to @article
+    end
+
+    def update
+        @article = Article.find(params[:id])
+ 
+        if @article.update(article_params)
+           redirect_to @article
+        else
+           render 'edit'
+        end
+    end
+
+    private 
+      def article_params
+        params.require(:article).permit(:title,:text)
+      end
 end
